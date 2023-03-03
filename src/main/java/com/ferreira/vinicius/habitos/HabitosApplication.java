3e1bd6db45001b2a9f01a172c0e6d6bed48d3b00
vinicius ferreira <vinicius.arruda15eel@gmail.com>
2023-03-03 20:00:18 -0300

    versão inicial

---
+++ src/main/java/com/ferreira/vinicius/habitos/HabitosApplication.java
@@ -0,0 +1,13 @@
+package com.ferreira.vinicius.habitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitosApplication.class, args);
+	}
+
+}
