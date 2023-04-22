13f81bda163061d0cb9966ab885d1e3c9262a066
vinicius ferreira <vinicius.arruda15eel@gmail.com>
2023-04-22 14:43:37 -0300

    Implemetei "ResponseEntity" onde era cabivel.
    Adicionei um ID que é gerado automaticamente e alterei todos os endpoint para o padrão "/usuario/{id}".
    
    Comecei fazer requisições atravez do "RequestBody" em vez de "RequestParam",

--- src/main/java/com/ferreira/vinicius/habitos/Cadastro.java
+++ src/main/java/com/ferreira/vinicius/habitos/Cadastro.java
@@ -1,32 +1,40 @@
 package com.ferreira.vinicius.habitos;
+
 import java.util.Map;
 import java.util.Objects;
 import java.util.HashMap;
-public class Cadastro  {
+
+public class Cadastro {
 	private String usuario = "";
 	private String senha = "";
-	//Construtor
+	private String id;
+
+	// Construtor
 	public Cadastro() {
-		
+
 	}
-	
+
 	public Cadastro(String usuario, String senha) {
 		this.usuario = usuario;
 		this.senha = senha;
 	}
-	public String getUsuario(){
+
+	public String getUsuario() {
 		return this.usuario;
 	}
+
 	public void setUsuario(String usuario) {
-		this.usuario = usuario; 
-	}
-	public String getSenha(){
-		return this.senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha; 
+		this.usuario = usuario;
 	}
 
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	
+}
 	@Override
 	public int hashCode() {
 		return Objects.hash(senha, usuario);
@@ -43,7 +51,23 @@ public class Cadastro  {
 		Cadastro other = (Cadastro) obj;
 		return Objects.equals(senha, other.senha) && Objects.equals(usuario, other.usuario);
 	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String string) {
+		this.id = string;
+	}
+
+	public Object getloginRequest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	
+
 	
-	
+		
+
 }
--- src/main/java/com/ferreira/vinicius/habitos/HabitosLoginControlador.java
+++ src/main/java/com/ferreira/vinicius/habitos/HabitosLoginControlador.java
@@ -2,70 +2,78 @@
 
 package com.ferreira.vinicius.habitos;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HabitosLoginControlador {
 
-	
-
+	String conectado = "1";
 	Map<String, Cadastro> cadastroMap = new HashMap<String, Cadastro>();
-
+//onde por a classe Resquest
 	@PostMapping("/login")
-	public String login(@RequestParam String usuario, @RequestParam String senha) {
+	public ResponseEntity<Cadastro> login(@RequestBody LoginRequest loginRequest) {
 		
-		if (cadastroMap.containsKey(usuario) && cadastroMap.get(usuario).getSenha().equals(senha)) {
-			return " Acesso permitido";
-		} else if(!cadastroMap.containsKey(usuario)) {
-			return "Usuario inexistente";
-		}
-		else {
-			return "Acesso negado";
-		}
-	}
+		Cadastro usuario = cadastroMap.get(loginRequest.getUsuario());
+		usuario.setUsuario(loginRequest.getUsuario());
+		
+		if (usuario!= null && usuario.getloginRequest().equals(loginRequest.getSenha())) {
+			
+			return ResponseEntity.ok().build();
 
-	@PostMapping("/usuario")
-	public String salvarUsuario(@RequestParam String usuario, @RequestParam String senha) {
-		Cadastro novoUsuario = new Cadastro();
-		novoUsuario.setSenha(senha);
-		novoUsuario.setUsuario(usuario);
-		cadastroMap.put(usuario, novoUsuario);
-
-		return "Usuario criado com sucesso.";
-
-	}
-
-	@GetMapping("/listarUsuario")
-	public Collection<Cadastro> listarUsuarios() {
-	return cadastroMap.values();
-
-		}
-	
-	@DeleteMapping("/deletarUsuario")
-	public String deletarUsuario(@RequestParam String nomeUsuario) {
-
-		if (!cadastroMap.containsKey(nomeUsuario)) {
-			return "usuario inexistente";
 		} else {
-			cadastroMap.remove(nomeUsuario);
-			return "usuario " + nomeUsuario + " removido com sucesso";
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		}
 	}
-	@PostMapping("/atualizarUsuario")
-	public Collection<Cadastro> atualizarUsuario(){
-		return null;
+
+//Por que não preciso criar um novo usuario?  ou aplicar
+	@PostMapping("/usuarios")
+	public ResponseEntity<Cadastro> criarUsuario(@RequestBody Cadastro novoUsuario) {
+		UUID uuid = UUID.randomUUID();
+
+		novoUsuario.setId(uuid.toString());
+
+		cadastroMap.put(uuid.toString(), novoUsuario);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+	}
+
+	@GetMapping("/usuarios")
+	public Collection<Cadastro> listarUsuarios() {
+		return cadastroMap.values();
+	}
+
+	@DeleteMapping("/usuarios/{id}")
+	public ResponseEntity<Cadastro> deletarUsuario(@PathVariable String id) {
+		if (!cadastroMap.containsKey(id)) {
+			return ResponseEntity.notFound().build();
+		} else {
+			cadastroMap.remove(id);
+			return ResponseEntity.ok().build();
+		}
+	}
+
+	@PutMapping("/usuarios/{id}")
+	public ResponseEntity<Cadastro> atualizarCadastro(@PathVariable String id,
+			@RequestBody Cadastro usuarioAtualizado) {
+		Cadastro cadastro = cadastroMap.get(id);
+		if (cadastro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		cadastro.setSenha(usuarioAtualizado.getSenha());
+		cadastro.setUsuario(usuarioAtualizado.getUsuario());
+		return ResponseEntity.ok(cadastro);
 	}
 }
