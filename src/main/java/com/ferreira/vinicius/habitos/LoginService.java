4d6f4267f76696f5cb7031cafc45bb89292d4f18
vinicius ferreira <vinicius.arruda15eel@gmail.com>
2023-09-02 18:32:51 -0300

    foram implementados testes com jUnit

--- src/main/java/com/ferreira/vinicius/habitos/LoginService.java
+++ src/main/java/com/ferreira/vinicius/habitos/LoginService.java
@@ -1,5 +1,26 @@
 package com.ferreira.vinicius.habitos;
 
-public class LoginService {
+import java.util.HashMap;
+import java.util.Map;
 
+public class AccessControlManager {
+	private Map<String, Long> tempoDeAcesso;
+
+	public AccessControlManager() {
+		this.tempoDeAcesso = new HashMap<>();
+	}
+
+	public void registradorTempo(String idUsuario) {
+		tempoDeAcesso.put(idUsuario, System.currentTimeMillis());
+	}
+
+	public  boolean acessoPermitido(String idUsuario) {
+		Long ultimoAcesso = tempoDeAcesso.get(idUsuario);
+		if (ultimoAcesso != null) {
+			long horaAtual = System.currentTimeMillis();
+			long tempoDecorrido = horaAtual - ultimoAcesso;
+			return tempoDecorrido <= 30 * 60 * 1000;
+		}
+		return false;
+	}
 }
